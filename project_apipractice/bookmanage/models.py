0d9ce3f6238ae5701adae2ae1c0b7624140c186b
Dhruvraj <dhruvrajsinh.zala@etatvasoft.com>
2024-09-06 18:17:06 +0530

    commit on 06/09 at 18.16

---
+++ project_apipractice/bookmanage/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class BookData(models.Model):
+    book_name = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255)
+    book_price= models.DecimalField(max_digits=5, decimal_places=2)
+    published_date = models.DateField()
+    copies_available= models.IntegerField()
+    available_sw_cpy= models.BooleanField(default=False)
+
